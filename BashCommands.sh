67771e6fc50d6e98d5896efcec211b09b081cfd6
Pranjalirawke <36563474+Pranjalirawke@users.noreply.github.com>
2023-05-08 21:13:01 +0700

    Update BashCommands.sh
    updated code

--- BashCommands.sh
+++ BashCommands.sh
@@ -1,54 +1,32 @@
 #!/bin/bash
 
-# 1. open cmd
+# Connect to remote server
+ssh ubuntu@control.brandsonroad.com 
 
-ssh ubuntu@control.brandsonroad.com << EOF
-
-mongodump --forceTableScan --db led --gzip --archive=mongoBackup_`date +"%Y-%m-%d.gz"`
-
+cd ~
+mongodump --forceTableScan --db led --gzip --archive=mongoBackup_$(date +"%Y-%m-%d.gz")
 ll
-
-EOF
-
+cd F:mongoDB
 scp ubuntu@control.brandsonroad.com:mongoBackup_2021-08-02.gz ./
-
-ssh brodata2@192.168.7.209:/home/siladmin/mongo-db << EOF
-
+ssh brodata2@192.168.7.209 '/home/siladmin/mongo-db'
 docker ps -a
-
 docker ps
-
 cd ..
-
 cd siladmin/
-
 ls
-
 cd mongo-db
-
 docker-compose down
-
 docker volume ls
-
-docker volume rm <volume name>
-
-EOF
-
-docker cp F:/mongo/filename.gz container_name:/path/to/destination
-
-
-scp Filename brodata2@192.168.7.209:/home/siladmin/mongo-db
-
-brodata2@192.168.7.209:/home/siladmin/mongo-db
-
+docker volume rm <volume name> #replace the volume name
+cd ..
+cd ..
+scp F:mongo/Filename brodata2@192.168.7.209:/home/siladmin/mongo-db #replace the filename
+ssh brodata2@192.168.7.209 '/home/siladmin/mongo-db'
 scp .\mongoBackup_2022-07-29.gz siladmin@192.168.7.209:~/mongo-db/mongoBackup.gz
-
 ll
-
-mv Filename.gz mongoBackup.gz
-
+mv Filename.gz mongoBackup.gz #replce the filenam
 docker-compose up -d
-
-cd ..cd ..
-
+cd ..
+cd ..
 docker ps
+
